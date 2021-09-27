ae0de41e0ebcf67070a9d1ded1711f6be90b34bb
Anton Kuzmin <Anton_Kuzmin4@epam.com>
2021-09-27 09:44:33 +0300

    use java home for tests if present

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,5 +1,5 @@
 import os
 
-os.environ['JAVA_HOME'] = '/usr/local/opt/openjdk@8'
+os.environ['JAVA_HOME'] = os.environ.get('JAVA_HOME') or '/usr/local/opt/openjdk@8'
 os.environ['JAVA_OPTS'] = '-Dio.netty.tryReflectionSetAccessible=true'
 
